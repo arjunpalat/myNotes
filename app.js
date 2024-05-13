c36de3a421508e9f5cd9911b11e62dc583b4698c
arjunpalat <palathinkaraarjun@gmail.com>
2024-05-13 21:40:51 +0530

    Add Cron Scheduler to ping render to prevent sleep down

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const middleware = require("./utils/middleware");
 const logger = require("./utils/logger");
 const mongoose = require("mongoose");
 const apiRouter = require("./controllers/api");
+const cron = require("node-cron");
+import("node-fetch");
 
 mongoose.set("strictQuery", false);
 
@@ -22,13 +24,27 @@ mongoose
     logger.error("Error connecting to MongoDB:", error.message);
   });
 
+  const { PING_URLS, MINUTES } = require("./utils/config");
+
+  cron.schedule(`*/${MINUTES} * * * *`, async () => {
+    try {
+      for (const url of PING_URLS) {
+        const response = await fetch(url);
+        if (response.ok) {
+          console.log(`Pinged at ${new Date()}`);
+        }
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  });
+
 app.use(cors());
 app.use(express.static("dist"));
 app.use(express.json());
 app.use(middleware.requestLogger);
 
 app.use("/api", apiRouter);
-
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
--- utils/config.js
+++ utils/config.js
@@ -2,8 +2,12 @@ require("dotenv").config();
 
 const PORT = process.env.PORT;
 const MONGODB_URI = process.env.MONGODB_URI;
+const PING_URLS = process.env.PING_URLS.split(" ");
+const MINUTES = process.env.MINUTES;
 
 module.exports = {
   MONGODB_URI,
   PORT,
+  PING_URLS,
+  MINUTES,
 };
